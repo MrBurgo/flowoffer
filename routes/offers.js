c96e47a8a7149d1fd4569c5e6e97fdf433274c42
Nathan Burgo <nthnbrg185@gmail.com>
2018-04-02 14:41:06 -0600

    better route handling

---
+++ routes/offers.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+const router = express.Router()
+
+// GET all offers
+router.get('/', (req, res, next) => {
+  res.send('GET an offer')
+})
+
+// GET a single offer
+router.get('/:id', (req, res, next) => {
+  res.send('GET a single offer')
+})
+
+// POST a new offer
+router.post('/', (req, res, next) => {
+  res.send('POST a new offer')
+})
+
+// DELETE an offer
+router.delete('/:id', (req, res, next) => {
+  res.send('DELETE an offer')
+})
+
+// UPDATE an offer
+router.patch('/:id', (req, res, next) => {
+  res.send('UPDATE an offer')
+})
+
+module.exports = router
